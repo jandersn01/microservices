f7ffa7816407c2e0439ab926a73ab92118f211ab
janderson01 <jandersonsantos030@gmail.com>
2026-07-01 02:10:44 -0300

    inicia pratica 5

--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -1,12 +1,19 @@
 package payment_adapter
 
 import (
+	"log"
 	"context"
+	"time"
+
 
 	"github.com/jandersn01/microservices-proto/golang/payment"
 	"github.com/jandersn01/microservices/order/internal/application/core/domain"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
+
 )
 
 type Adapter struct {
@@ -15,7 +22,15 @@ type Adapter struct {
 
 func NewAdapter (paymentServiceUrl string) (*Adapter, error) {
 	var opts []grpc.DialOption
-	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts,
+		grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(
+			grpc_retry.WithCodes(codes.Unavailable, codes.ResourceExhausted),
+			grpc_retry.WithMax(5),
+			grpc_retry.WithBackoff(grpc_retry.BackoffLinear(time.Second)),
+
+		)))
+	opts = append(opts, grpc.WithInsecure())
+
 	conn, err := grpc.Dial(paymentServiceUrl, opts...)
 	if err != nil {
 		return nil, err
@@ -25,11 +40,20 @@ func NewAdapter (paymentServiceUrl string) (*Adapter, error) {
 }
 
 func (a *Adapter) Charge(order *domain.Order) error {
-	_, err := a.payment.Create(context.Background(), &payment.CreatePaymentRequest{
+	ctx,_ := context.WithTimeout(context.Background(), 2 *time.Second)
+	
+	_, err := a.payment.Create(ctx, &payment.CreatePaymentRequest{
 		UserId: order.CustomerID,
 		OrderId: order.ID,
 		TotalPrice: order.TotalPrice(),
 
 	})
-	return err	
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.DeadlineExceeded {
+			log.Println("A requisição para o microsserviço Payment falhou: tempo limite de 2 segundos excedido (DeadlineExceeded).")
+		}
+		return err
+	}
+	return nil	
 }
---
+++ shipping/go.mod
@@ -0,0 +1,3 @@
+module github.com/jandersn01/microservices/shipping
+
+go 1.26.1
